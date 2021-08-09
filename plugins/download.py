224cbd0c01f3a8e1252dd72af1e4ee9be986a775
samadii <77415087+samadii@users.noreply.github.com>
2021-08-09 14:21:58 +0430

    Update download.py

--- plugins/download.py
+++ plugins/download.py
@@ -39,29 +39,32 @@ LOGGER = logging.getLogger(__name__)
     total,
     ud_type,
     message,
-    start,
-    filename
+    start
 ):
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
+        # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        elapsed_time = time_formatter(milliseconds=elapsed_time)
-        estimated_total_time = time_formatter(milliseconds=estimated_total_time)
-        progress = "○ **Name :** `{}`\n".format(filename)
-        progress += "[{0}{1}] \n○ **Percentage :** `{2}%`\n○ **Completed :** ".format(
+
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+
+        progress = "[{0}{1}] \nP: {2}%\n".format(
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
             ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 1))
-        tmp = progress + "`{0}` of `{1}`\n○ **Speed :** `{2}/s`\n○ **ETA :** `{3}`\n".format(
+            round(percentage, 2))
+
+        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            estimated_total_time if estimated_total_time != '' else "0 s",
+            # elapsed_time if elapsed_time != '' else "0 s",
+            estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
             await message.edit(
@@ -70,7 +73,7 @@ LOGGER = logging.getLogger(__name__)
                     tmp
                 )
             )
-        except MessageNotModified:
+        except:
             pass'''
 
 # --- HUMANBYTES DEF --- #
